e1a4b176f074bf9198c385374d72f0666b42005d
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-27 00:18:46 +0700

    create fun TopAppBarKontak in KontakApp

--- app/src/main/java/com/example/roomdatabaseapi/KontakApp.kt
+++ app/src/main/java/com/example/roomdatabaseapi/KontakApp.kt
@@ -1,9 +1,14 @@
 package com.example.roomdatabaseapi
 
+import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -58,4 +63,26 @@ fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modi
         },
         modifier = modifier
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                }
+            }
+        })
 }
