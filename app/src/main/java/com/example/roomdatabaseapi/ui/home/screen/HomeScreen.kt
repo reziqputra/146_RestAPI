35b73981c72c70a3ab08c09c9624b911e34104aa
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-27 01:00:13 +0700

    modify HomeScreen to HomeStatus and fix error

--- app/src/main/java/com/example/roomdatabaseapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/roomdatabaseapi/ui/home/screen/HomeScreen.kt
@@ -34,13 +34,19 @@ import com.example.roomdatabaseapi.model.Kontak
 import com.example.roomdatabaseapi.ui.home.viewmodel.KontakUIState
 
 @Composable
-fun HomeScreen(
-    kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier
+fun HomeStatus(
+    kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier, onDeleteClick: (Kontak) -> Unit = {}, onDetailClick: (Int) -> Unit
 ) {
     when (kontakUIState) {
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
         is KontakUIState.Success -> KontakLayout(
-            kontak = kontakUIState.kontak, modifier = modifier.fillMaxWidth()
+            kontak = kontakUIState.kontak, modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id)
+            },
+            onDeleteClick = {
+                onDeleteClick(it)
+            }
         )
 
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
