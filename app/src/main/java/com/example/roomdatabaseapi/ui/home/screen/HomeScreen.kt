8ed308ca3cdea86c00cd4a3629877192b8fe8611
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-27 00:49:23 +0700

    modify fun KontakCard

--- app/src/main/java/com/example/roomdatabaseapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/roomdatabaseapi/ui/home/screen/HomeScreen.kt
@@ -14,11 +14,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -110,7 +112,14 @@ fun KontakCard(
                 Icon(imageVector = Icons.Default.Phone, contentDescription = null)
                 Text(text = kontak.telpon, style = MaterialTheme.typography.titleMedium)
             }
-            Text(text = kontak.alamat, style = MaterialTheme.typography.titleMedium)
+            Row (modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically)
+            {
+                Text(text = kontak.alamat, style = MaterialTheme.typography.titleMedium)
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(kontak)}) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                }
+            }
         }
     }
 }
