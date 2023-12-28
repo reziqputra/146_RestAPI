6180ec93eb56b89106a3b815c79ab0e68edbde32
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-28 18:21:01 +0700

    create class InsertViewModel

--- app/src/main/java/com/example/roomdatabaseapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/roomdatabaseapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,27 @@
 package com.example.roomdatabaseapi.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabaseapi.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
