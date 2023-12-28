511e0d35798becade9020e6129bd0937a78117bd
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-28 18:26:11 +0700

    create data class InsertUiState and InsertUiEvent

--- app/src/main/java/com/example/roomdatabaseapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/roomdatabaseapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -2,6 +2,7 @@ package com.example.roomdatabaseapi.ui.kontak.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.roomdatabaseapi.repository.KontakRepository
@@ -25,3 +26,14 @@ class InsertViewModel(private val kontakRepository: KontakRepository) : ViewMode
         }
     }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
